b2e97bb6c392a841b913186d9216b9ede2d894e1
Eduardo Machado <eduardo.silvamachado07@gmail.com>
2024-09-04 01:14:31 -0300

    configura branch

--- index_Make/src/pages/negocios.jsx
+++ index_Make/src/pages/negocios.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Negocios = () => {
   return (
     <div className='w-full mt-3'>
-        <h1 className='text-center text-3xl'>Todas os nossos estabelecimentos</h1>
+        <h1 className='text-center text-3xl'>Todas os nossos estabelecimentos e mais</h1>
     </div>
   )
 }
